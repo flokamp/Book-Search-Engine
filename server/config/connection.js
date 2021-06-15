8a70ad673dbe04666ce4b63fdc5088609c37f38e
flokamp <74511633+flokamp@users.noreply.github.com>
2021-06-14 19:54:22 -0500

    plz work

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,3 +8,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/book-search", {
 });
 
 module.exports = mongoose.connection;
+
+VjgdpZgu4YifVww
+
+mongodb+srv://flokamp:VjgdpZgu4YifVww@cluster0.9ipuq.mongodb.net/google_books?retryWrites=true&w=majority
+
+mongodb+srv://flokamp:VjgdpZgu4YifVww@cluster0.9ipuq.mongodb.net/book_search?retryWrites=true&w=majority
